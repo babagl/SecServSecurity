eb5cbc84d30519dfb9bf853226e2ce1ea6331fb2
Abdoulaye Aliou SALL <87236495+babagl@users.noreply.github.com>
2022-10-11 20:57:03 +0100

    Add files via upload

--- SecServSecurity/src/main/java/com/spring/secservsecurity/filter/JwtAuthenticationFilter.java
+++ SecServSecurity/src/main/java/com/spring/secservsecurity/filter/JwtAuthenticationFilter.java
@@ -14,6 +14,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
+import java.util.stream.Collectors;
 
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -39,8 +41,14 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
         System.out.println("successfulAuthentication");
         User user = (User) authResult.getPrincipal();
-        Algorithm algorithm = Algorithm.HMAC256("mySecret123");
-        String jwtAccessToken = JWT.create();
-        super.successfulAuthentication(request, response, chain, authResult);
+        Algorithm algorithm1 = Algorithm.HMAC256("mySecret123");
+        String jwtAccessToken = JWT.create()
+                .withSubject(user.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis()+5*60*1000))
+                .withIssuer(request.getRequestURL().toString())
+                .withClaim("roles",user.getAuthorities().stream().map(ga -> ga.getAuthority()).collect(Collectors.toList()))
+                .sign(algorithm1);
+
+        response.setHeader("Authorization",jwtAccessToken);
     }
 }
